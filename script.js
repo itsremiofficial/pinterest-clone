f756543d9468c5a08918e969d1bf7a6c1d92b91b
REMI <84964174+itsremiofficial@users.noreply.github.com>
2024-01-06 23:57:40 +0500

    updates

--- script.js
+++ script.js
@@ -1,30 +1,6 @@
-function loco() {
-    gsap.registerPlugin(ScrollTrigger);
-
-    const locoScroll = new LocomotiveScroll({
-        el: document.querySelector("#main"),
-        smooth: true
-    });
-
-    locoScroll.on("scroll", ScrollTrigger.update);
-
-    ScrollTrigger.scrollerProxy("#main", {
-        scrollTop(value) {
-            return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
-        },
-        getBoundingClientRect() {
-            return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight };
-        },
-
-        pinType: document.querySelector("body").style.transform ? "transform" : "fixed"
-    });
-
-    ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
-
-    ScrollTrigger.refresh();
-}
-
-loco();
+document.querySelector('#main').scrollIntoView({ 
+    behavior: 'smooth' 
+  });
 
 const sections = ['decor', 'fashion', 'plants', 'food'];
 const texts = document.querySelectorAll('.text');
